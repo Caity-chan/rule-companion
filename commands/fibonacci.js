2302e707da01516385f181a96a3349cddaf90467
companion <pandonautical@gmail.com>
2020-11-12 23:07:17 +0000

    added arguments for Fibonacci command

--- commands/fibonacci.js
+++ commands/fibonacci.js
@@ -3,16 +3,20 @@ module.exports = {
 	description: 'fibonacci!',
 	execute(message, args, client) {
     // fibonacci = 1 2 3 5 8 13 21 34 55 89....
+    na = args[0];
+    nb = args[1];
+    seconds = args[2];
+    //message.channel.send(na);
     function F(na, nb, seconds){
       setTimeout(function() {
         val = na + nb; //first value + second value = next number in fibonacci sequence
         na = nb; //set first value to second value
         nb = val; //set second value to current number in fibonacci sequence
-        console.log(val); //logs the current number in the fibonacci sequence
+        message.channel.send(val); //logs the current number in the fibonacci sequence
         F(na, nb, seconds); //do the function again with the new values
       }, seconds * 1000); //1000 milliseconds is 1 second, the delay function interprets the value as milliseconds so you need to multiply the value by 1000 
     }
 
-    F(0, 1, 1); //starts out at values 0 and 1 at a delay of 1 second per turn
+    F(na, nb, seconds); //starts out at values 0 and 1 at a delay of 1 second per turn
 	},
 };
