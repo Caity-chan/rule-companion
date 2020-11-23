0a7607d757a428a09b734e8aaab76fe26ac0bea2
companion <pandonautical@gmail.com>
2020-11-23 21:58:05 +0000

    made demo branch

--- commands/owner/id.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'fetch-user',
-	description: 'fetch a user!',
-	execute(message, args, client) {
-    console.log(args[0])
-		client.users.fetch(args[0]).then(user=>message.channel.send(user.tag))
-  }
-};
